fe27c0c536fcc7c3dbf2bb420eff4153d529a8bb
shinuhyun <shinuhyun@gmail.com>
2020-08-31 16:14:05 +0900

    :sparkles: [추가] postPasswordMypage API 작성

--- controllers/mypage/postPasswordMypage.js
+++ controllers/mypage/postPasswordMypage.js
@@ -1,4 +1,27 @@
-const db = require("../../models");
+const jwt = require("jsonwebtoken");
+const { User } = require("../../models");
 
-//
-module.exports = {};
+module.exports = {
+  post: async (req, res) => {
+    const token = req.headers["x-access-token"];
+    const { password } = req.body;
+    try {
+      const decoded = await jwt.verify(token, req.app.get("jwt-secret"));
+      const result = await User.findOneByEmailAndPassword(
+        decoded.email,
+        password
+      );
+      // 존재하지 않는 유저
+      if (result === null) {
+        res.status(404).json({ message: "Wrong Password" });
+      } else {
+        // 24 hours
+        res.status(200).json({ message: "Password Correct!" });
+      }
+    } catch (err) {
+      // server error handling
+      console.log(err);
+      res.status(500).json({ message: "Server Error" });
+    }
+  },
+};
