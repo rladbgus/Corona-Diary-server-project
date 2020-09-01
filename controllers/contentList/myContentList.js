00f8dac4c03fc07b031773e9209335e54ac8510e
shinuhyun <shinuhyun@gmail.com>
2020-09-01 15:17:07 +0900

    :sparkles: [추가] myContentList API 작성

---
+++ controllers/contentList/myContentList.js
@@ -0,0 +1,20 @@
+const { Content } = require("../../models");
+const jwt = require("jsonwebtoken");
+
+module.exports = {
+  get: async (req, res) => {
+    try {
+      const token = req.headers["x-access-token"];
+      const decoded = await jwt.verify(token, req.app.get("jwt-secret"));
+
+      const myContentList = await Content.findAll({
+        attributes: ["id", "title", "text", "updatedAt"],
+        where: { userId: decoded.id },
+      });
+      res.status(200).send({ contentList: myContentList });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Server Error");
+    }
+  },
+};
