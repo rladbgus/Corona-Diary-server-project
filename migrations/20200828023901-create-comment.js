67e70886b6543fcbf64f3c7366f2049970ef7f5e
gunwooko <kkw722@gmail.com>
2020-09-01 17:31:07 +0900

    :sparkles: [추가] 한글 작성 가능하게 추가

--- migrations/20200828023901-create-comment.js
+++ migrations/20200828023901-create-comment.js
@@ -1,48 +1,55 @@
 "use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("Comments", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      comment: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      userId: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        references: {
-          model: {
-            tableName: "Users",
-            key: "id",
+    await queryInterface.createTable(
+      "Comments",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        comment: {
+          type: Sequelize.TEXT,
+          allowNull: false,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        userId: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+          references: {
+            model: {
+              tableName: "Users",
+              key: "id",
+            },
+            onDelete: "CASCADE",
           },
-          onDelete: "CASCADE",
+        },
+        contentId: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+          references: {
+            model: {
+              tableName: "Contents",
+              key: "id",
+            },
+            onDelete: "CASCADE",
+          },
         },
       },
-      contentId: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        references: {
-          model: {
-            tableName: "Contents",
-            key: "id",
-          },
-          onDelete: "CASCADE",
-        },
-      },
-    });
+      {
+        charset: "utf8mb4",
+        collate: "utf8mb4_general_ci",
+      }
+    );
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable("Comments");
