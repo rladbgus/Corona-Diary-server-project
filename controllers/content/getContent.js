b9837df8d967c6698d8894105c790fba918adb80
gunwooko <kkw722@gmail.com>
2020-09-01 13:48:33 +0900

    :sparkles: [추가] getContent 추가 - 라우터 주소값 변경 - makeContent에서 contentId도 같이 넘겨주기로함

--- controllers/content/getContent.js
+++ controllers/content/getContent.js
@@ -6,5 +6,42 @@ module.exports = {
   get: (req, res) => {
     const token = req.headers["x-access-token"];
     const decoded = jwt.verify(token, req.app.get("jwt-secret"));
+    const { contentId } = req.params;
+
+    try {
+      db.Content.findAll({
+        where: { id: contentId },
+        attributes: [
+          "id",
+          "title",
+          "text",
+          "covid_date",
+          "referenceFile",
+          "like",
+        ],
+        include: [
+          { model: db.User, as: "user", attributes: ["id", "nickName"] },
+          {
+            model: db.Comment,
+            as: "comment",
+            attributes: ["id", "comment", "createdAt"],
+            include: [
+              { model: db.User, as: "user", attributes: ["id", "nickName"] },
+            ],
+          },
+        ],
+      }).then((contentDatail) => {
+        if (contentDatail) {
+          res.status(200).send({ message: "성공!", result: contentDatail });
+        } else {
+          res.status(404).send("잘못된 요청입니다.");
+        }
+      });
+    } catch (err) {
+      (err) => {
+        console.log(err);
+        res.status(500).json({ message: "Server Error" });
+      };
+    }
   },
 };
