8a4d773da2554895b4f509f2f4c4a25e06fcd2de
gunwooko <kkw722@gmail.com>
2020-09-02 20:42:33 +0900

    :hammer: [수정] patchContent에서 태그 업데이트 추가 및 수정 - 수정: makeContent에서 태그 중복 처리, 태그와 유저의 조인테이블 중복 처리 - User_Tags 조인테이블 마이그레이션 수정: tagId에 unique 속성 달아줌 - 수정: patchContent에서 태그 업데이트 하는 부분 수정

--- controllers/content/patchContent.js
+++ controllers/content/patchContent.js
@@ -19,6 +19,7 @@ module.exports = {
       q_sleep,
       q_fatigue,
       q_psy,
+      tags,
     } = req.body;
 
     try {
@@ -41,6 +42,43 @@ module.exports = {
             { where: { id: content.id, userId: decoded.id } }
           ).then((contentUpdated) => {
             if (contentUpdated) {
+              // 해당 태그를 삭제하면, 유저 그리고 컨텐츠와의 관계는 삭제된다.
+              db.Content_Tag.findAll({ where: { contentId: contentId } }).then(
+                (tags) => {
+                  tags.map((eachTag) => {
+                    db.Content_Tag.destroy({
+                      where: { contentId: contentId, tagId: eachTag.tagId },
+                    }).then();
+                  });
+                }
+              );
+              tags.map((eachTag) => {
+                // findOrCreate 사용해서 태그 중복 활용하기
+                db.Tag.findOrCreate({
+                  where: { tag: eachTag },
+                  defaults: { tag: eachTag },
+                })
+                  .then(([eachTagDetail, created]) => {
+                    // 태그와 유저의 조인테이블에 저장
+                    db.User_Tag.findOrCreate({
+                      where: { tagId: eachTagDetail.id },
+                      defaults: {
+                        userId: decoded.id,
+                        tagId: eachTagDetail.id,
+                      },
+                    }).then(([eachTagDetail2, created]) => {
+                      // 태그와 컨텐츠의 조인테이블에 저장
+                      db.Content_Tag.create({
+                        contentId: contentId,
+                        tagId: eachTagDetail2.tagId,
+                      }).then();
+                    });
+                  })
+                  .catch((err) => {
+                    console.error(err);
+                    res.status(500).send("Server Error");
+                  });
+              });
               res.status(201).send({ message: "수정되었습니다" });
             } else {
               res.status(404).send("잘못된 요청입니다");
