3de09545747d90735e98abac00131abab7277a2a
gunwooko <kkw722@gmail.com>
2020-09-02 16:31:32 +0900

    :hammer: [수정] makeContent API에서 tag를 받아올 수 있도록 수정 추가: 태그 테이블과 유저 테이블 연결, 태그 테이블과 해당 컨텐츠 테이블과 연결 이슈: req.body로 tags:[태그이름1, 태그이름2, ...] 추가해서 보내주기

--- controllers/content/makeContent.js
+++ controllers/content/makeContent.js
@@ -18,46 +18,65 @@ module.exports = {
       q_sleep,
       q_fatigue,
       q_psy,
+      tags,
     } = req.body;
 
     try {
-      db.User.findOne({ where: { email: decoded.email } }).then((user) => {
-        if (user.id) {
-          db.Content.create({
-            title: title,
-            text: text,
-            covid_date: covid_date,
-            referenceFile: referenceFile,
-            q_temp: q_temp,
-            q_resp: q_resp,
-            q_cough: q_cough,
-            q_appet: q_appet,
-            q_sleep: q_sleep,
-            q_fatigue: q_fatigue,
-            q_psy: q_psy,
-            userId: user.id,
-          }).then((result) => {
-            if (result) {
-              res.status(201).send({
-                message: "게시글이 작성되었습니다.",
-                contentId: result.id,
-              });
-            } else {
-              res.status(404).send("잘못된 요청입니다.");
-            }
-          });
-        } else {
-          res.status(404).send("잘못된 요청입니다. user.id가 없음");
+      db.User.findOne({ where: { email: decoded.email, id: decoded.id } }).then(
+        (user) => {
+          if (user.id) {
+            db.Content.create({
+              title: title,
+              text: text,
+              covid_date: covid_date,
+              referenceFile: referenceFile,
+              q_temp: q_temp,
+              q_resp: q_resp,
+              q_cough: q_cough,
+              q_appet: q_appet,
+              q_sleep: q_sleep,
+              q_fatigue: q_fatigue,
+              q_psy: q_psy,
+              userId: user.id,
+            }).then((result) => {
+              if (result) {
+                // 태그 테이블에 저장
+                tags.map((eachTag) => {
+                  db.Tag.create({ tag: eachTag })
+                    .then((eachTagDetail) => {
+                      // 태그와 유저의 조인테이블에 저장
+                      db.User_Tag.create({
+                        userId: decoded.id,
+                        tagId: eachTagDetail.id,
+                      }).then((eachTagDetail2) => {
+                        // 태그와 컨텐츠의 조인테이블에 저장
+                        db.Content_Tag.create({
+                          contentId: result.id,
+                          tagId: eachTagDetail2.tagId,
+                        }).then();
+                      });
+                    })
+                    .catch((err) => {
+                      console.error(err);
+                      res.status(500).send("Server Error");
+                    });
+                });
+                res.status(201).send({
+                  message: "게시글이 작성되었습니다.",
+                  contentId: result.id,
+                });
+              } else {
+                res.status(404).send("잘못된 요청입니다.");
+              }
+            });
+          } else {
+            res.status(404).send("잘못된 요청입니다. user.id가 없음");
+          }
         }
-      });
+      );
     } catch (err) {
       console.log(err);
       res.status(500).json({ message: "Server Error" });
     }
   },
 };
-
-// 유저의 고유 id 접근 방법:
-// 1. 유저가 로그인을 했을 때, 해당 유저의 id 값을 건내서 준다.
-// => 그러면 해당 유저가 글을 작성할 때, 자신의 id를 바디에 함께 넘긴다.
-// 2. 토큰 디코디드의 이메일로 해당 유저를 찾아 id값을 찾아내서 해결한다.
