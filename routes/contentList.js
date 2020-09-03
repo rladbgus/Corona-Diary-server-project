e3f04c8908b540152658573756d2f023b5184a28
shinuhyun <shinuhyun@gmail.com>
2020-09-03 10:59:35 +0900

    :sparkles: [추가] mainContentList API 작성

--- routes/contentList.js
+++ routes/contentList.js
@@ -5,5 +5,6 @@ const { contentListController } = require("../controllers");
 router.get("/contentList", contentListController.contentList.get);
 router.get("/myContentList", contentListController.myContentList.get);
 router.get("/contentList/:tagId", contentListController.tagContentList.get);
+router.get("/mainContentList", contentListController.mainContentList.get);
 
 module.exports = router;
