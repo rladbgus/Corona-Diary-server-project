fd20888d8c9de2ea0fd6910feb5ee595c4dc7fca
Ubuntu <ubuntu@ip-172-31-43-251.ap-northeast-2.compute.internal>
2020-09-03 05:21:01 +0000

    deploy

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const contentListRouter = require("./routes/contentList");
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "http://corona-diary.s3-website.ap-northeast-2.amazonaws.com"],
     method: ["GET", "POST", "DELETE", "PATCH"],
     credentials: true,
   })
@@ -28,16 +28,16 @@ app.use(morgan("dev"));
 // set the secret key variable for jwt
 app.set("jwt-secret", config.secret);
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.use("/user", userRouter);
 app.use("/", authMiddleware);
 app.use("/mypage", mypageRouter);
 app.use("/content", contentRouter);
 app.use("/comment", commentRouter);
 app.use("/", contentListRouter);
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
