e1d524a456d148821e638e25808fbf4331152cde
shinuhyun <shinuhyun@gmail.com>
2020-09-04 14:53:17 +0900

    :hammer: [수정] createContent API 수정  태그 없어도 createContent 가능하게끔 수정

--- controllers/content/makeContent.js
+++ controllers/content/makeContent.js
@@ -22,65 +22,58 @@ module.exports = {
     } = req.body;
 
     try {
-      db.User.findOne({ where: { email: decoded.email, id: decoded.id } }).then(
-        (user) => {
-          if (user.id) {
-            db.Content.create({
-              title: title,
-              text: text,
-              covid_date: covid_date,
-              referenceFile: referenceFile,
-              q_temp: q_temp,
-              q_resp: q_resp,
-              q_cough: q_cough,
-              q_appet: q_appet,
-              q_sleep: q_sleep,
-              q_fatigue: q_fatigue,
-              q_psy: q_psy,
-              userId: user.id,
-            }).then((result) => {
-              if (result) {
-                // 태그 테이블에 저장
-                tags.map((eachTag) => {
-                  // findOrCreate 사용해서 태그 중복 활용하기
-                  db.Tag.findOrCreate({
-                    where: { tag: eachTag },
-                    defaults: { tag: eachTag },
-                  })
-                    .then(([eachTagDetail, created]) => {
-                      // 태그와 유저의 조인테이블에 저장
-                      db.User_Tag.findOrCreate({
-                        where: { tagId: eachTagDetail.id },
-                        defaults: {
-                          userId: decoded.id,
-                          tagId: eachTagDetail.id,
-                        },
-                      }).then(([eachTagDetail2, created]) => {
-                        // 태그와 컨텐츠의 조인테이블에 저장
-                        db.Content_Tag.create({
-                          contentId: result.id,
-                          tagId: eachTagDetail2.tagId,
-                        }).then();
-                      });
-                    })
-                    .catch((err) => {
-                      console.error(err);
-                      res.status(500).send("Server Error");
-                    });
-                });
-                res.status(201).send({
-                  message: "게시글이 작성되었습니다.",
-                  contentId: result.id,
-                });
-              } else {
-                res.status(404).send("잘못된 요청입니다.");
-              }
+      const content = await db.Content.create({
+        title: title,
+        text: text,
+        covid_date: covid_date,
+        referenceFile: referenceFile,
+        q_temp: q_temp,
+        q_resp: q_resp,
+        q_cough: q_cough,
+        q_appet: q_appet,
+        q_sleep: q_sleep,
+        q_fatigue: q_fatigue,
+        q_psy: q_psy,
+        userId: decoded.id,
+      });
+      if (!content) {
+        res.status(404).send("잘못된 요청입니다.");
+      } else {
+        if (tags.length === 0) {
+          res.status(201).send({
+            message: "게시글이 작성되었습니다.",
+            contentId: content.id,
+          });
+        } else {
+          tags.map((eachTag) => {
+            // findOrCreate 사용해서 태그 중복 활용하기
+            db.Tag.findOrCreate({
+              where: { tag: eachTag },
+              defaults: { tag: eachTag },
+            }).then(([eachTagDetail, created]) => {
+              // 태그와 유저의 조인테이블에 저장
+              db.User_Tag.findOrCreate({
+                where: { tagId: eachTagDetail.id },
+                defaults: {
+                  userId: decoded.id,
+                  tagId: eachTagDetail.id,
+                },
+              }).then();
+              db.Content_Tag.findOrCreate({
+                where: { tagId: eachTagDetail.id, contentId: content.id },
+                defaults: {
+                  contentId: content.id,
+                  tagId: eachTagDetail.id,
+                },
+              }).then();
             });
-          } else {
-            res.status(404).send("잘못된 요청입니다. user.id가 없음");
-          }
+          });
+          res.status(201).send({
+            message: "게시글이 작성되었습니다.",
+            contentId: content.id,
+          });
         }
-      );
+      }
     } catch (err) {
       console.log(err);
       res.status(500).json({ message: "Server Error" });
