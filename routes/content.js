e758329d16bcebd2d5fdf248688dc3b0c91cbe60
gunwooko <kkw722@gmail.com>
2020-09-07 17:07:48 +0900

    :hammer: [수정] deleteContentAPI 수정 - 수정내용: 일기 삭제할 때, s3에 저장된 이미지파일도 함께 저장하게끔 수정 - 추가: 미들웨어에서 s3 이미지 삭제 처리해줌

--- routes/content.js
+++ routes/content.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { contentController } = require("../controllers");
 const { fileUploadMiddleware } = require("../middlewares/s3FileUpload");
+const { fileDeleteMiddleware } = require("../middlewares/s3FileDelete");
 
 // 작성된 글을 불러올 때 사용합니다.
 router.get("/:contentId", contentController.getContent.get);
@@ -13,6 +14,7 @@ router.post("/:contentId/like", contentController.plusLike.post);
 router.patch("/:contentId", contentController.patchContent.patch);
 
 // 글을 삭제할 때 사용합니다.
+router.use("/:contentId", fileDeleteMiddleware);
 router.delete("/:contentId", contentController.deleteContent.delete);
 
 // 글을 작성할 때 사용합니다.
