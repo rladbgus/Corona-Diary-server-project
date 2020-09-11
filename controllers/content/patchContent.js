74559b4ad7cb6b0b2d9b530431b86d594e54fc69
shinuhyun <shinuhyun@gmail.com>
2020-09-11 16:23:41 +0900

    :broken_heart: [버그] patchContent API 수정 tags에 "undefined"가 작성되는 오류 발생 => 수정

--- controllers/content/patchContent.js
+++ controllers/content/patchContent.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 
 // 글을 수정할 때 사용합니다.
 module.exports = {
-  patch: (req, res) => {
+  patch: async (req, res) => {
     const token = req.headers["x-access-token"];
     const decoded = jwt.verify(token, req.app.get("jwt-secret"));
     const { contentId } = req.params;
@@ -20,82 +20,77 @@ module.exports = {
       q_psy,
       tags,
     } = req.body;
-    const tagArr = tags.split(",");
 
     try {
       let bin = req.file;
       if (!bin) {
         bin = "";
       }
+      await db.Content.update(
+        {
+          title: title,
+          text: text,
+          covid_date: covid_date,
+          referenceFile: bin.location,
+          q_temp: q_temp,
+          q_resp: q_resp,
+          q_cough: q_cough,
+          q_appet: q_appet,
+          q_sleep: q_sleep,
+          q_fatigue: q_fatigue,
+          q_psy: q_psy,
+        },
+        { where: { id: contentId, userId: decoded.id } }
+      );
 
-      db.Content.findOne({ where: { id: contentId, userId: decoded.id } })
-        .then((content) => {
-          db.Content.update(
+      if (!tags || tags === "undefined") {
+        res
+          .status(201)
+          .send({ message: "게시글이 수정되었습니다.", contentId: contentId });
+      } else {
+        await db.Tag.findAll({
+          include: [
             {
-              title: title,
-              text: text,
-              covid_date: covid_date,
-              referenceFile: bin.location,
-              q_temp: q_temp,
-              q_resp: q_resp,
-              q_cough: q_cough,
-              q_appet: q_appet,
-              q_sleep: q_sleep,
-              q_fatigue: q_fatigue,
-              q_psy: q_psy,
+              model: db.Content,
+              as: "content",
+              where: { id: contentId },
+              attributes: [],
+              through: {
+                attributes: [],
+              },
             },
-            { where: { id: content.id, userId: decoded.id } }
-          ).then((contentUpdated) => {
-            if (contentUpdated) {
-              // 해당 태그를 삭제하면, 유저 그리고 컨텐츠와의 관계는 삭제된다.
-              db.Content_Tag.findAll({ where: { contentId: contentId } }).then(
-                (tags) => {
-                  tags.map((eachTag) => {
-                    db.Content_Tag.destroy({
-                      where: { contentId: contentId, tagId: eachTag.tagId },
-                    }).then();
-                  });
-                }
-              );
-
-              tagArr.map((eachTag) => {
-                // findOrCreate 사용해서 태그 중복 활용하기
-                db.Tag.findOrCreate({
-                  where: { tag: eachTag },
-                  defaults: { tag: eachTag },
-                })
-                  .then(([eachTagDetail, created]) => {
-                    // 태그와 유저의 조인테이블에 저장
-                    db.User_Tag.findOrCreate({
-                      where: { tagId: eachTagDetail.id },
-                      defaults: {
-                        userId: decoded.id,
-                        tagId: eachTagDetail.id,
-                      },
-                    }).then(([eachTagDetail2, created]) => {
-                      // 태그와 컨텐츠의 조인테이블에 저장
-                      db.Content_Tag.create({
-                        contentId: contentId,
-                        tagId: eachTagDetail2.tagId,
-                      }).then();
-                    });
-                  })
-                  .catch((err) => {
-                    console.error(err);
-                    res.status(500).send("Server Error");
-                  });
-              });
-              res
-                .status(201)
-                .send({ message: "수정되었습니다", contentId: contentId });
-            } else {
-              res.status(404).send("잘못된 요청입니다");
-            }
-          });
-        })
-        .catch((err) => {
-          console.error("contentId 값이 잘못되었습니다.", err);
+          ],
+        }).then((results) => {
+          results.map((result) => result.destroy());
         });
+
+        const tagArr = tags.split(",");
+
+        tagArr.map((eachTag) => {
+          db.Tag.findOrCreate({
+            where: { tag: eachTag },
+            defaults: { tag: eachTag },
+          }).then(([eachTagDetail, created]) => {
+            db.User_Tag.findOrCreate({
+              where: { tagId: eachTagDetail.id },
+              defaults: {
+                userId: decoded.id,
+                tagId: eachTagDetail.id,
+              },
+            }).then();
+            db.Content_Tag.findOrCreate({
+              where: { tagId: eachTagDetail.id, contentId: contentId },
+              defaults: {
+                contentId: contentId,
+                tagId: eachTagDetail.id,
+              },
+            }).then();
+          });
+        });
+        res
+          .status(201)
+          .send({ message: "게시글이 수정되었습니다.", contentId: contentId });
+      }
     } catch (err) {
       (err) => {
         console.log(err);
